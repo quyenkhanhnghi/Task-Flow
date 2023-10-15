1fcfaa737dfb137e8650d5d5526643dc6b68e78a
Nghi Quyen <quyenkhanhnghi317@gmail.com>
2023-10-15 04:00:26 +0300

    change module import

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import MyDialog from "@/components/Dialog";
+import MyDialog from "../components/Dialog";
 
 const inter = Inter({ subsets: ["latin"] });
 
